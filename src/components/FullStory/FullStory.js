c014003cf9ef573d7b0c83632d8e32edf25684f9
Stephanie Guzman <sguzman247@gmail.com>
2023-01-09 18:19:36 -0600

    Connect FullStory component to App

---
+++ src/components/FullStory/FullStory.js
@@ -0,0 +1,9 @@
+import './FullStory.css'
+
+export const FullStory = () => {
+  return (
+    <section className='full-story-container'>
+      <h1>This is the full story container</h1>
+    </section>
+  )
+}
