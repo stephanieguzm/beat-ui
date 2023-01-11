0e6f955766ffabaedf2bbb3043ed9d6a1f26ebbd
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 12:40:58 -0600

    Create new component to capture user section selection

---
+++ src/components/Form/Form.js
@@ -0,0 +1,17 @@
+import './Form.css'
+
+// get user section selection
+// pass section up to App
+// useEffect() to re-render results
+// then passed down to Stories
+
+export const Form = ({ setSectionSelection, sections }) => {
+  
+  // const sectionOptions = 
+
+  return (
+    <form className='form-container'>
+      
+    </form>
+  )
+}
