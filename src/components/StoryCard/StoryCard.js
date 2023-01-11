e47b9caabeba0b8aad84e1052eb20eff2e4c1e7f
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 09:46:44 -0600

    Add props, Link, and styling to story card component

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,10 +1,17 @@
+import { Link } from 'react-router-dom'
 import './StoryCard.css'
 
 export const StoryCard = ({ story }) => {
 
+  console.log(story)
   return (
-    <section className='story-card'>
-      <h1>{story.title}</h1>
-    </section>
+    <Link to={`/${story.id}`}>
+      <section className='story-card' id={story.id}>
+        <h1>{story.title}</h1>
+        <p>{story.section}</p>
+        <p>{story.byline}</p>
+        <p>{story.longUrl}</p>
+      </section>
+    </Link>
   )
 }
