249f342c0339c2c2420cb696695ca1c6536ac6f7
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 11:46:02 -0600

    Reorder HTML elements + add NYT full story link

--- src/components/FullStory/FullStory.js
+++ src/components/FullStory/FullStory.js
@@ -7,14 +7,14 @@ console.log(selectedStory)
   return (
     <section className='full-story-container' id={selectedStory.id}>
       <h1>{selectedStory.title}</h1>
-      <p>{selectedStory.abstract}</p>
       <p>{selectedStory.byline}</p>
       <p>{selectedStory.section}</p>
       <img src={selectedStory.multimedia[1].url} alt={selectedStory.multimedia[1].caption} />
       <p>{selectedStory.multimedia[1].caption}</p>
+      <p>{selectedStory.abstract}</p>
       <p>Published: {selectedStory.publishedDate}</p>
       {selectedStory.updatedDate && <p>Updated: {selectedStory.updatedDate}</p>}
-
+      <a href={selectedStory.longUrl} target='_blank' rel='noreferrer'> READ THE FULL STORY</a>
     </section>
   )
 }
