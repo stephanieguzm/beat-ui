8dd22ea68d15b5d9b55260808526c0026a4a91bd
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 12:40:29 -0600

    Remove dead code

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -2,14 +2,13 @@ import { Link } from 'react-router-dom'
 import './StoryCard.css'
 
 export const StoryCard = ({ story }) => {
-  console.log(story)
   return (
     <Link to={`/${story.id}`}>
       <section className='story-card' id={story.id}>
         <img className='card-img' src={story.multimedia[1].url} alt={story.multimedia[1].caption} />
         <h2>{story.title}</h2>
-        <p>{story.section}</p>
         <p>{story.byline}</p>
+        <p>{story.section}</p>
       </section>
     </Link>
   )
