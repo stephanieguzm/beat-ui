e612729d05a85a38b9b32771219bb9764f25e8eb
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 21:04:37 -0600

    Add conditional rendering to for multimedia prop with null value

--- src/components/FullStory/FullStory.js
+++ src/components/FullStory/FullStory.js
@@ -1,20 +1,21 @@
+import nyTimesLogo from '../../nyt_logo.jpeg'
 import './FullStory.css'
 
 export const FullStory = ({ topStories, storyId }) => {
   const selectedStory = topStories.find(story => story.id === storyId) 
-console.log(selectedStory)
 
   return (
     <section className='full-story-container' id={selectedStory.id}>
       <h1>{selectedStory.title}</h1>
       <p>{selectedStory.byline}</p>
       <p>{selectedStory.section}</p>
-      <img src={selectedStory.multimedia[1].url} alt={selectedStory.multimedia[1].caption} />
-      <p>{selectedStory.multimedia[1].caption}</p>
+      {!selectedStory.multimedia && <img src={nyTimesLogo} alt={selectedStory.title} />}
+      {selectedStory.multimedia && <img src={selectedStory.multimedia[1].url} alt={selectedStory.multimedia[1].copyright} />}
+      {selectedStory.multimedia && <p>{selectedStory.multimedia[1].caption}</p>}
       <p>{selectedStory.abstract}</p>
       <p>Published: {selectedStory.publishedDate}</p>
       {selectedStory.updatedDate && <p>Updated: {selectedStory.updatedDate}</p>}
-      <a href={selectedStory.longUrl} target='_blank' rel='noreferrer'> READ THE FULL STORY</a>
+      {selectedStory.longUrl && <a href={selectedStory.longUrl} target='_blank' rel='noreferrer'> READ THE FULL STORY</a>}
     </section>
   )
 }
