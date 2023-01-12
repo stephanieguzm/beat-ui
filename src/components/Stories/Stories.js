c49a179a1e4f2a07a05c720d8b20e8fca5a1715a
Stephanie Guzman <sguzman247@gmail.com>
2023-01-11 21:05:31 -0600

    Pass individual story props to story cards in order to conditionally render card info

--- src/components/Stories/Stories.js
+++ src/components/Stories/Stories.js
@@ -1,11 +1,17 @@
 import { StoryCard } from '../StoryCard/StoryCard'
+import nyTimesLogo from '../../nyt_logo.jpeg'
 import './Stories.css'
 
 export const Stories = ({ topStories }) => {
 
   const storyCards = topStories.map(story => {
     return <StoryCard 
-      story={story}
+      id={story.id}
+      title={story.title}
+      byline={story.byline}
+      section={story.section}
+      img={!story.multimedia ? nyTimesLogo : story.multimedia[1].url}
+      imgCaption={!story.multimedia ? story.title : story.multimedia[1].copyright}
       key={story.id}
     />
   }) 
