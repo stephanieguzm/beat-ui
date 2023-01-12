75253d7a2b0af6710488cba46e12ccf5e8376ab3
Stephanie Guzman <sguzman247@gmail.com>
2023-01-12 12:38:55 -0600

    Add Link route to route user to home page + add unique keys to sections

--- src/components/Sections/Sections.js
+++ src/components/Sections/Sections.js
@@ -1,21 +1,26 @@
+import { nanoid } from 'nanoid'
+import { Link } from 'react-router-dom'
 import './Sections.css'
 
 export const Sections = ({ setUserSelectedSelection }) => {
 
   const sections = ["home", "arts", "automobiles", "books", "business", "fashion", "food", "health", "insider", "magazine", "movies", "nyregion", "obituaries", "opinion", "politics", "realestate", "science", "sports", "sundayreview", "technology", "theater", "t-magazine", "travel", "upshot", "us", "world"]  
+  
   const handleClick = (event) => {
     setUserSelectedSelection(event.target.value)
   }
   
   const sectionOptions = sections.map(section => {
     return (
-      <button
-        className='section'
-        value={section}
-        onClick={(event) => handleClick(event)}
-        key={section}>
-        {section.toUpperCase()}
-      </button>
+      <Link to='/'>
+        <button
+          className='section'
+          value={section}
+          onClick={(event) => handleClick(event)}
+          key={nanoid()}>
+          {section.toUpperCase()}
+        </button>
+      </Link>
     )
   })
 
