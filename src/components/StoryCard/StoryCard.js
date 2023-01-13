5165267b323ff51a0397f273b1a578391b085efd
Stephanie Guzman <sguzman247@gmail.com>
2023-01-12 20:33:29 -0600

    Add styling + classNames + remove unused props

--- src/components/StoryCard/StoryCard.js
+++ src/components/StoryCard/StoryCard.js
@@ -1,15 +1,14 @@
 import { Link } from 'react-router-dom'
 import './StoryCard.css'
 
-export const StoryCard = ({ id, title, byline, section, img, imgCaption }) => {
+export const StoryCard = ({ id, title, byline, img, imgCaption }) => {
 
   return (
     <section className='story-card' id={id}>
       <Link to={`/${id}`}>
         <img className='card-img' src={img} alt={imgCaption} />
-        <h2>{title}</h2>
-        <p>{byline}</p>
-        <p>{section}</p>
+        <h2 className='card-text'>{title}</h2>
+        <p className='card-text'>{byline}</p>
       </Link>
     </section>
   )
